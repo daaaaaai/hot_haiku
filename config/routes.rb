fab0f3f8d9e7b6b962dbf255320175df1ba410db
katayama <katayama@on-the-slope.com>
2022-03-21 13:13:39 +0900

    利用規約追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'haikus#index'
-  resources :haikus, only: [:show, :index, :create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'haikus#index'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'term' => 'static_pages#term'
+  resources :haikus, only: [:show, :index, :create]
 end
